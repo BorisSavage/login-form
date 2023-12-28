eb4e4fe1607591f58234f9e62a33254a5a2d343a
Michal Borysewicz <michael.borissavage@gmail.com>
2023-12-28 16:34:22 +0100

    create navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import { Logo } from "./Logo";
+import { BarsArrowDownIcon, XMarkIcon } from "@heroicons/react/24/solid";
+
+const HOME_ROUTE = "/";
+const BUY_ROUTE = "/buy";
+const SELL_ROUTE = "/sell";
+const ABOUT_ROUTE = "/about";
+const LOGIN_ROUTE = "/login";
+const REGISTER_ROUTE = "/register";
+
+export function Navbar() {
+  const [show, setShow] = React.useState(false);
+
+  return (
+    <>
+      <div
+        className={cn(
+          "absolute transition hidden duration-500 top-0 left-0 h-[100dvh] w-[100dvw] z-10",
+          {
+            "backdrop-blur-sm block": show,
+          },
+        )}
+        onClick={() => setShow(!show)}
+      />
+      <div className="absolute w-fit flex items-center justify-start z-50 top-0 left-0">
+        <nav
+          className={cn(
+            "relative -top-24 transition-all opacity-0 blur-lg duration-500 ease-in-out w-fit flex flex-col h-[100svh] items-center justify-start px-2 pt-1 pb-8 text-sm",
+            // "sm:px-1 sm:text-base",
+            "after:absolute after:right-0 after:top-0 after:block after:w-[1px] after:h-full after:bg-gradient-to-t after:from-neutral-900 after:via-neutral-900/50 after:to-neutral-900 after:opacity-80 after:transition after:duration-500 after:content-['']",
+            "after:hover:opacity-100",
+            {
+              "top-0 opacity-100 blur-none": show,
+            },
+          )}
+        >
+          <div className="flex h-10 items-center justify-start text-sm">
+            <Link
+              className="flex items-center justify-start pr-3 font-bold text-slate-900 transition duration-500 dark:text-slate-100 sm:pl-2 sm:pr-5"
+              href="/"
+            >
+              <Logo />
+              ACME
+            </Link>
+            <div className="absolute -bottom-10 right-0 sm:static"></div>
+          </div>
+          <NavigationMenu className="flex flex-col items-center justify-between">
+            <NavigationMenuList className="flex flex-col space-y-2 space-x-0 justify-center items-center">
+              <NavigationMenuItem>
+                <Link href={HOME_ROUTE} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Home
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <Link href={BUY_ROUTE} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Buy
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href={SELL_ROUTE} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Sell
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href={ABOUT_ROUTE} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    About
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+            <NavigationMenuList className="flex flex-col space-y-2 space-x-0 justify-center items-center">
+              <NavigationMenuItem>
+                <Link href={REGISTER_ROUTE} legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={cn(
+                      navigationMenuTriggerStyle(),
+                      "border border-neutral-200 bg-neutral-100",
+                    )}
+                  >
+                    Register
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href={LOGIN_ROUTE} legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={cn(
+                      navigationMenuTriggerStyle(),
+                      "border border-neutral-400 bg-neutral-300",
+                    )}
+                  >
+                    Login
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </nav>
+        <XMarkIcon
+          className={cn(
+            "absolute top-1/2 right-0 -translate-y-1/2 translate-x-[200%] transition duration-500 blur-lg opacity-0 pointer-events-none h-8 w-8",
+            {
+              "opacity-100 blur-none translate-x-[150%]": show,
+            },
+          )}
+        />
+      </div>
+      <BarsArrowDownIcon
+        onClick={() => setShow(true)}
+        className={cn(
+          "h-8 w-8 backdrop-blur-md z-50 backdrop-opacity-50 rounded-sm fixed bottom-4 left-4 text-neutral-900 transition-all duration-500",
+          {
+            "opacity-0 translate-y-4 blur-lg": show,
+          },
+        )}
+      />
+    </>
+  );
+}
