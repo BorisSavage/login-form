d5d5177809cae6f7e10b35d75d2960d20d732819
Michal Borysewicz <michael.borissavage@gmail.com>
2023-12-28 13:19:46 +0100

    Install Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import {Button} from '@nextui-org/button'; 
-
+import { Button } from "@nextui-org/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-
-      
       <Button>Click me</Button>
-
-      
     </main>
-  )
+  );
 }
