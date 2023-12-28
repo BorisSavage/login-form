01c0be46ec3ead29d691a0762fd189e209aab53b
Michal Borysewicz <michael.borissavage@gmail.com>
2023-12-28 23:30:30 +0100

    Intall zod and react-hook-form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,27 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
 export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-center text-3xl font-bold">Login</h1>
